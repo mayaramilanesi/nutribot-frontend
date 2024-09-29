691444a184ce1420a97537c6f4965b5a14f51f66
Mayara Milaneesi <mayaramilanesim@gmail.com>
2024-09-29 10:23:04 -0300

    fix: test change

---
+++ .releaserc.js
@@ -0,0 +1,8 @@
+export default {
+    branches: ["master"],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github"
+    ]
+}
